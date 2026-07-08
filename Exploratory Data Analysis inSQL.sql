bb86e5462022f583b23a0377290b2ef10ca95d30
Williams Ezekiel <williamsezekiel350@gmail.com>
2026-07-08 17:21:23 +0000

    Add exploratory data analysis SQL

--- Exploratory Data Analysis inSQL.sql
+++ Exploratory Data Analysis inSQL.sql
@@ -1,192 +1,81 @@
-#Data Cleaning
-
-
-select *
-from layoffs;
-
-#Remove Duplicates
-#Standardize
-#Null or Blank Values
-#Remove Any columns or Rows
-
-
-CREATE TABLE layoffs_staging
-LIKE layoffs;
+#Exploratory Data Analysis
 
 select *
-from layoffs_staging;
+from layoffs_staging2;
 
-INSERT layoffs_staging
-SELECT *
-FROM layoffs;
+select max(total_laid_off), max(percentage_laid_off)
+from layoffs_staging2;
 
 select *
-from layoffs_staging;
+from layoffs_staging2
+where percentage_laid_off = 1
+order by funds_raised_millions desc;
 
-select *,
-row_number () over(
-partition by company, location, industry, total_laid_off, percentage_laid_off, `date`,stage, country, funds_raised_millions) as row_num
-from layoffs_staging;
-with duplicate_cte as
-(select *,
-row_number () over(
-partition by company, location, industry, total_laid_off, percentage_laid_off, `date`,stage, country, funds_raised_millions) as row_num
-from layoffs_staging
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+select min(`date`), max(`date`)
+from layoffs_staging2;
+
+select industry, sum(total_laid_off)
+from layoffs_staging2
+group by industry
+order by 2 desc;
+
+select country, sum(total_laid_off)
+from layoffs_staging2
+group by country
+order by 2 desc;
+
+select year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by year(`date`)
+order by 1 desc;
+
+select industry, max(percentage_laid_off)
+from layoffs_staging2
+group by industry
+order by 2 desc;
+
+
+select stage, sum(total_laid_off)
+from layoffs_staging2
+group by stage
+order by 1 desc;
+
+select substring(`date`, 1,7) as `month`, sum(total_laid_off)
+from layoffs_staging2
+where substring(`date`, 1,7) is not null
+group by `month`
+order by 1 asc;
+
+with Rollimg_Total as 
+(select substring(`date`, 1,7) as `month`, sum(total_laid_off) as total_off
+from layoffs_staging2
+where substring(`date`, 1,7) is not null
+group by `month`
+order by 1 asc
 )
-select *
-from duplicate_cte
-where row_num > 1;
+select `month`, total_off,
+sum(total_off) over(order by `month`) as rolling_total
+from Rollimg_Total;
 
-select *
-from layoffs_staging
-where company ='Casper';
 
-with duplicate_cte as
-(
-select *,
-row_number () over(
-partition by company, location, industry, total_laid_off, percentage_laid_off, `date`,stage, country, funds_raised_millions) as row_num
-from layoffs_staging
-)
-delete
-from duplicate_cte
-where row_num > 1;
-
-CREATE TABLE `layoffs_staging2` (
-  `company` text,
-  `location` text,
-  `industry` text,
-  `total_laid_off` int DEFAULT NULL,
-  `percentage_laid_off` text,
-  `date` text,
-  `stage` text,
-  `country` text,
-  `funds_raised_millions` int DEFAULT NULL,
-  `row_num` INT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-select *
+select company, year(`date`), sum(total_laid_off)
 from layoffs_staging2
-where row_num = 2;
+group by company, year(`date`)
+order by 3 desc;
 
-INSERT INTO layoffs_staging2
-select *,
-row_number () over(
-partition by company, location, industry, total_laid_off, percentage_laid_off, `date`,stage, country, funds_raised_millions) as row_num
-from layoffs_staging;
-
-
-
-SET SQL_SAFE_UPDATES = 0;
-
-delete
+with company_year(company, years, total_laid_off) as
+(select company, year(`date`), sum(total_laid_off)
 from layoffs_staging2
-where row_num > 1;
-
+group by company, year(`date`)
+),  company_year_rank as
+(select *, dense_rank() over(partition by years order by total_laid_off desc) as ranking
+from company_year
+where years is not null)
 select *
-from layoffs_staging2
-where row_num > 1;
-
-SET SQL_SAFE_UPDATES = 1;
-
-select *
-from layoffs_staging2;
-
-#STANDARDIZE DATA
-select company, TRIM(company)
-from layoffs_staging2;
-
-SET SQL_SAFE_UPDATES = 0;
-UPDATE layoffs_staging2
-SET company = TRIM(company);
-
-
-
-select *
-from layoffs_staging2
-where industry like 'Crypto%';
-
-select distinct location
-from layoffs_staging2;
-
-select distinct country
-from layoffs_staging2
-order by 1;
-
-select distinct country, TRIM(TRAILING '.' FROM country)
-from layoffs_staging2
-order by 1;
-
-UPDATE layoffs_staging2
-SET country = TRIM(TRAILING '.' FROM country)
-where country like 'United States%';
-
-select `date`
-from layoffs_staging2;
-
-update layoffs_staging2
-set`date` = str_to_date(`date`, '%m/%d/%Y');
-
-alter table layoffs_staging2
-modify column `date` date;
-
-select *
-from layoffs_staging2
-where total_laid_off is null
-and percentage_laid_off is null;
-
-select *
-from layoffs_staging2
-where industry is null
-or industry = '';
-
-select *
-from layoffs_staging2
-where company = 'Airbnb';
-
-select *
-from layoffs_staging2 t1
-join layoffs_staging2 t2
-        on t1.company = t2.company
-        and t1.location = t2.location
-where (t1.industry is null or t1.industry = '')
-and t2.industry is not null;
-
-
-select t1.industry, t2.industry
-from layoffs_staging2 t1
-join layoffs_staging2 t2
-        on t1.company = t2.company
-        and t1.location = t2.location
-where (t1.industry is null or t1.industry = '')
-and t2.industry is not null;
-
-update layoffs_staging2 t1
-join layoffs_staging2 t2
-    on t1.company = t2.company
-set t1.industry = t2.industry
-where (t1.industry is null or t1.industry = '')
-and t2.industry is not null;
-
-update layoffs_staging2
-set industry = null
-where industry ='';
-
-select *
-from layoffs_staging2;
-
-select *
-from layoffs_staging2
-where total_laid_off is null
-and percentage_laid_off is null;
-
-delete
-from layoffs_staging2
-where total_laid_off is null
-and percentage_laid_off is null;
-
-select *
-from layoffs_staging2;
-
-alter table layoffs_staging2
-drop column row_num;
+from company_year_rank
+where ranking <= 5;
